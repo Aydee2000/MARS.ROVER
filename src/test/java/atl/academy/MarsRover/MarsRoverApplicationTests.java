afec3a6fabcf210ea2fcd89581b30c019e55c835
Aydee2000 <aydeeysabeltoledo@gmailcom>
2023-10-08 22:18:04 -0300

    adding files

---
+++ src/test/java/atl/academy/MarsRover/MarsRoverApplicationTests.java
@@ -0,0 +1,13 @@
+package atl.academy.MarsRover;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarsRoverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
