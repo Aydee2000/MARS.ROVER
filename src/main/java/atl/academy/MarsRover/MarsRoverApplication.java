6f75d3f3d94ef93b6dece1dff658f0e64c1697cb
Aydee2000 <aydeeysabeltoledo@gmailcom>
2023-10-08 22:18:51 -0300

    adding files

---
+++ src/main/java/atl/academy/MarsRover/MarsRoverApplication.java
@@ -0,0 +1,13 @@
+package atl.academy.MarsRover;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarsRoverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarsRoverApplication.class, args);
+	}
+
+}
