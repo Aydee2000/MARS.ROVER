e9d13511483f8b00e30823dd825e368bf63ee150
Aydee2000 <aydeeysabeltoledo@gmailcom>
2023-10-10 18:34:51 -0300

    adding repository

--- src/main/java/atl/academy/MarsRover/Repository/ObstacleRepository.java
+++ src/main/java/atl/academy/MarsRover/Repository/ObstacleRepository.java
@@ -3,6 +3,8 @@ package atl.academy.MarsRover.Repository;
 import atl.academy.MarsRover.Models.Obstacle;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface
-ObstacleRepository extends JpaRepository<Obstacle, Long> {
+
+
+public interface ObstacleRepository extends JpaRepository<Obstacle, Long> {
+
 }
